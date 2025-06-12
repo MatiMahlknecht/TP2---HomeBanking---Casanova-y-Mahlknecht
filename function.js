a03adcc2a35cf630f63f5a19f8720f4a1f535d64
MatiMahlknecht <mmahlknecht@pioix.edu.ar>
2025-06-12 08:49:10 -0300

    Hicimos el punto 9

--- function.js
+++ function.js
@@ -1,7 +1,25 @@
-function findClient(id){
-    for(let i = 0; i<clients.length; i++){
-        if (id==clients[i].id){
+function findClient(id) {
+    for (let i = 0; i < clients.length; i++) {
+        if (id == clients[i].id) {
             return i
         }
     }
 }
+
+
+function findSavingBanks(id, typeCurrency) {
+    let associatesSavingBanks = []
+    for (let i = 0; i < clients.length; i++) {
+        for (let j = 0; j < clients[i].savingBanks.length; j++) {
+            if (clients[i].id == id) {
+                if (typeCurrency == client[i].savingBanks[j].currency) {
+                    associatesSavingBanks.push(clients[i].savingBanks[j])
+                } else if (typeCurrency == undefined) {
+                    associatesSavingBanks.push(clients[i].savingBanks[j])
+                }
+            }
+        }
+    }
+    return associatesSavingBanks
+}
+
