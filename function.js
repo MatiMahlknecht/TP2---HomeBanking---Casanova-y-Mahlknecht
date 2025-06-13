5327dea37efa8ebe3c6bdb34d1ecfcdb38d34d92
Juan-Lucas07 <jcasanova@pioix.edu.ar>
2025-06-13 20:33:33 -0300

    Prueba-homebanking V0.2
    Hasta el punto 14 (incluido) terminado.

--- function.js
+++ function.js
@@ -23,3 +23,52 @@ function findSavingBanks(id, typeCurrency) {
     return associatesSavingBanks
 }
 
+//En caso de que no funncione mandarle 3 fors
+
+function findDebitCards(id) {
+    let associatesDebitCards = []
+    for (let i = 0; i < clients.length; i++) {
+        for (let j = 0; j < clients[i].savingBanks.length; j++) {
+            for (let k = 0; k < clients[i].savingBanks[j].debitCards.length; k++) {
+                if (clients[i].id == id) {
+                    associatesDebitCards.push(clients[i].savingBanks[j].debitCards[k])
+                }
+            }
+        }
+    }
+    return associatesDebitCards
+}
+
+function findDebitCardByIdCard(id){
+    for (let i = 0; i < clients.length; i++) {
+        for (let j = 0; j < clients[i].savingBanks.length; j++) {
+            for (let k = 0; k < clients[i].savingBanks[j].debitCards.length; k++) {
+                if (clients[i].savingBanks[j].debitCards[k].id == id) {
+                    return clients[i].savingBanks[j].debitCards[k]
+                }
+            }
+        }
+    }
+}
+
+function findCreditCards(id) {
+    let associatesCreditCards = []
+    for (let i = 0; i < clients.length; i++) {
+        for (let j = 0; j < clients[i].creditCards.length; j++) {
+            if(clients[i].creditCards[j] == id){
+                associatesCreditCards.push(clients[i].creditCards[j])
+            }
+        }
+    }
+    return associatesDebitCards
+}
+
+function findCreditCardByIdCard(id){
+for (let i = 0; i < clients.length; i++) {
+        for (let j = 0; j < clients[i].creditCards.length; j++) {
+            if(clients[i].creditCards[j].id == id){
+                return clients[i].creditCards[j]
+            }
+        }
+    }
+}
